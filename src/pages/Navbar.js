a1037a1f00408c7c493e3ce6fe3b9ec317014be4
Mac <mac@Mac.local>
2022-10-28 11:41:21 +0200

    adding link to the navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div className='w-full h-16 bg-blue-600 flex items-center justify-between px-10 py-2'>
-      <h1 className='text-white text-3xl font-semibold '>Crud</h1>
-      <button className='w-48 h-12 bg-white text-blue-500 font-semibold rounded-lg cursor-pointer hover:bg-transparent hover:border-2 hover:bg-dark hover:text-white  '>
-        Add Users
-      </button>
+
+      <Link to='/' className='text-white text-3xl font-semibold '>Crud</Link>
+      <Link to='add-user'>
+        <button className='w-48 h-12 bg-white text-blue-500  font-semibold rounded-lg cursor-pointer hover:bg-transparent hover:border-2 hover:bg-dark hover:text-white  '>
+          Add Users
+        </button>
+      </Link>
     </div>
   );
 };
